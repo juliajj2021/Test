61aa32e204adc0c3f2a79c11557c449e99898500
juliajj2021 <juliajhalstead@live.co.uk>
2021-10-22 17:24:39 +0100

    updates to bike

--- README.md
+++ README.md
@@ -7,7 +7,16 @@ so that i can use a good bike
 i'd like to see if a bike is working
 
 docking stations - release a bike
+docking station - dock a bike
 
 bikes - in working order
+
+as a user 
+so that i can return bikes that i have hired
+i want to be able to dock my bike at the docking station
+
+as a user
+so that i can decide whether to use this docking station
+i want to know there is a bike there
 
 
--- bike.py
+++ bike.py
@@ -4,6 +4,16 @@ class Bike:
 
 
 class Docking:
-    def docking_release(self):
-        print("Station will release")
-        return Bike()
+    def __init__(self):
+        self.docked_bikes = []
+
+    def release_bike(self):
+        if not self.docked_bikes:
+            print("There are no bikes to release")
+        else:
+            print("Station will release")
+            return self.docked_bikes.pop()
+        
+
+    def dock_bike(self, bike):
+        self.docked_bikes.append(bike)        
