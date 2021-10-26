1956cb5e704baaf324481e0093730ea7351a3095
juliajj2021 <juliajhalstead@live.co.uk>
2021-10-26 19:05:39 +0100

    Added broken bikes

--- README.md
+++ README.md
@@ -19,4 +19,11 @@ as a user
 so that i can decide whether to use this docking station
 i want to know there is a bike there
 
+as a user 
+so that i can reduce the chance of getting a broken bike in the future
+i'd like to report a bike as broken when i return it 
+
+as a maintainer f the system
+so that i can manage the bikes and not disapoint my users 
+i'd like to docking stations not to release broken bikes
 
--- bike.py
+++ bike.py
@@ -1,19 +1,40 @@
 class Bike:
+    ## Define constuctor for Bike
+    def  __init__(self):
+        self.bike_status = True 
+
     def is_working(self):
         print("bike is working")
 
 
 class Docking:
+    ## Set the constructor for this class
     def __init__(self):
         self.docked_bikes = []
 
     def release_bike(self):
+        for ind, i in self.docked_bikes:
+            if i.bike_status is True:
+                print("Station will release")
+                return self.docked_bikes.pop(ind)
         if not self.docked_bikes:
             print("There are no bikes to release")
-        else:
-            print("Station will release")
-            return self.docked_bikes.pop()
-        
 
-    def dock_bike(self, bike):
-        self.docked_bikes.append(bike)        
+
+    # for every docked bike in self.docked.bikes 
+    # check the status
+    # if bike is working then release
+    # but if no bikes working or there are no bikes - then we want to return no bikes available 
+
+
+
+    def dock_bike(self, bike, status):
+        bike.bike_status = status
+        self.docked_bikes.append(bike)
+
+        
+            
+            
+
+
+                        
